ffa5a9f815dc44d0aef61741080560e45b67421c
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-12 11:49:02 +0000

    datatype basics

---
+++ basic/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // by writing this we are telling that treat all code as JS newer version
+
+// alert("Aniket") it will not work here bcz we are using node not browser
+
+// code readibility is important. we can use ";" at the end of the line and not use also.
+
+// javascript main document "https://tc39.es/ecma262/". but genally use is MDN docs.
+
+let name = "Aniket"
+let age = 1
+let isGood = true
+
+age = age + 23 // just checking
+
+console.log(age);
+
+/* 
+There is many data types
+integer >= number
+float >= decimal number
+string >= "" use for nameing
+undefined >= 
+null >= standalone value
+symbol >= uniqueness
+
+object>=
+
+*/
+
+console.log(typeof 3); // by the use of typeof we check which data type it is
+
+console.log(typeof undefined); // output undefined
+console.log(typeof null); // output object

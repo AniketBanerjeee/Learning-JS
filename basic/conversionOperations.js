7b391a3b08862fbfc23349a1a23f701e3d7fb84a
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-12 12:24:03 +0000

    Conversion of Datatypes

---
+++ basic/conversionOperations.js
@@ -0,0 +1,36 @@
+let score = true
+
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+// "33" >= 33 for number it is easily convertable
+// "33abc" >= NaN - not an number
+// true >= 1 / false >= 0
+// null >= 0
+// undefined >= NaN
+
+let isGood = "a"
+
+let changeToBoolean = Boolean(isGood)
+//console.log(changeToBoolean);
+
+// 1 or any number positive or negetive > true ; 0 > false
+// "" = false empty string is false otherwise true
+
+
+let num = 77
+console.log(typeof num);
+console.log(num)
+
+
+let changeToString = String(num)
+console.log(typeof changeToString);
+console.log(changeToString);
+
+// We can also change number to string it look same but the type will change to different.

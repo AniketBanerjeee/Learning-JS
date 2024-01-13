061c8331facc1103ad6e9745b124bd2563482e70
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-13 06:11:10 +0000

    Comparison

---
+++ basic/comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 > 3);
+// this is basic number check this is normal easy and common.
+
+
+// console.log("2" == 2); // output is true
+// for this string two is getting converted and bcz the both are 2 it will show true
+
+console.log("2" === 2); // output is false
+// === is strict check it not only check the value also check the datatype so diffent datatype that's why it's false
+
+
+
+// console.log(null > 0);
+// console.log(undefined > 0);
+// comparison in null and undefined mostly we are avoid this bcz this give us confusion.
+

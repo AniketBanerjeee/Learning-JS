eb3a3ec0969eafb8e2606c749bb29eb5e5204388
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-15 10:42:11 +0000

    Sting using backtick

---
+++ basic/string.js
@@ -0,0 +1,11 @@
+let name = "Aniket"
+let number = 1234323
+
+// console.log("My name is " + name + ". My number is " + number);
+// This is the old way of concating or writing any string we should use back tick `` this
+
+console.log(`My name is ${name}. My number is ${number}`);
+
+// this is the easy way and modern way of writing string.
+
+

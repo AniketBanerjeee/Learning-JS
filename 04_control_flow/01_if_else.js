69abc2ea74b27c16820d1e0eb78f04295cd6c40d
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-22 12:13:32 +0000

    If else basics

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,71 @@
+// if(condition){}. if the condition is true it will go to the block
+// if we write else statement then it will print else block statement
+// let num = 5
+
+// if (num < 5){
+//     console.log(`Less than 5`)
+// } else {
+//     console.log(`greater than or equal to 5`);
+// }
+
+
+// we can check many condition by writing more if statement "else if(condition)"
+
+// let num = 15
+
+// if (num < 5){
+//     console.log(`Less than 5`)
+// } else if (num == 5) {
+//     console.log(`equal to 5`);
+// } else{
+//     console.log("greater than 5");
+// }
+
+
+// operator we can use
+// >, <, >=, <=, ==, !=, ===, !===
+
+
+
+
+// we can write if statement in one line also
+// let num = 5
+// if (num === 5) console.log("It is 5")
+
+// we can write like this also but
+// it is not easy to read so not in common practice 
+// let num = 6
+// if (num > 5) if (num < 10) console.log("greater than 5"), console.log("less than 10");
+
+
+
+
+// When we want to check multiple condition at the same time we use and and or operator.
+
+
+// && if both the thing is true it will execute
+// const userName = true
+// const password = false
+
+// if (userName && password){
+//     console.log("Welcome user");
+// } else{
+//     console.log("Please enter both");
+// }
+
+// || if one is true it will execute
+
+// const gmailAccount = true
+// const googleAccount = false
+
+// if (gmailAccount || googleAccount){
+//     console.log("Welcome user");
+// } else{
+//     console.log("You atleast need google or gmail account");
+// }
+
+
+
+
+
+

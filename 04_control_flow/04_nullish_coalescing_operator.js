7d11e526d23c2b07868926b665c9c069d885729e
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-22 12:56:50 +0000

    Nullish Coalescing Operator and Ternary operator

---
+++ 04_control_flow/04_nullish_coalescing_operator.js
@@ -0,0 +1,33 @@
+// Nullish Coalescing Operator (??) : null, undefined
+// if 1st value is null or undefined it will execute the 2nd thing
+
+// let val;
+
+// val = 5 ?? 10; // o/p - 5, if it is not null or undefined it will print the 1st value
+// val = null ?? 7 // o/p - 7, bcz if 1st is null it will execute the 2nd thing
+// val = undefined ?? 50 // o/p - 50, bcz if 1st is undefined it will execute the 2nd thing
+
+
+// console.log(val);
+
+//-----------------------------------------------------------------------------------------
+
+// ternary operator // checking in one line
+//condition? true: false
+
+let val1 = 11;
+
+val1 < 10 ? console.log("Less than 10"): console.log("greater than 10")
+
+
+
+
+
+
+
+
+
+
+
+
+

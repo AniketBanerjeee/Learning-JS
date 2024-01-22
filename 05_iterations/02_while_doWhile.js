b9c24e63c126b6d6f09b0b20d1320e06384bbb68
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-01-22 18:30:00 +0000

    while and do-while

---
+++ 05_iterations/02_while_doWhile.js
@@ -0,0 +1,40 @@
+// while loop. work is same as for loop syntax is different
+// we 1st initialise outer the loop 
+// while writing loop we give the condition
+// in the loop we give the increment of decrement value
+
+// let num = 1
+// while (num <= 15) {
+//     console.log(num);
+//     num = num + 1
+// }
+
+
+
+
+// printing array through while
+
+// let myArr = ["superman", "batman", "flash"]
+// let count = 0
+// while (count < myArr.length){
+//     console.log(myArr[count]);
+//     count++
+// }
+
+
+
+
+
+
+// do while loop
+
+let score = 13
+do {
+    // console.log(score);
+    score ++
+} while (score <= 12);
+
+// do- while is an exceptional case where we check the condition later in while but execute do part anyhow
+// in this case it will print 13 bcz it will execute the do part and check the condition
+
+

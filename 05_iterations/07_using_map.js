6a1da48a1011b8f87ade9bbbb5cc41862bd8b0a2
AniketBanerjeee <80254027+AniketBanerjeee@users.noreply.github.com>
2024-02-07 09:03:28 +0000

    Using map to loop

---
+++ 05_iterations/07_using_map.js
@@ -0,0 +1,18 @@
+// using map // map return the values so we can store the return value.
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const values = myNumber.map( (nums) => nums + 10 )
+// console.log(values)
+
+
+
+// chaining- we can use multiple map() and also filter() at the same time
+
+// const values = myNumber
+//                 .map((num) => num * 10)
+//                 .map((num) => num + 1)
+//                 .filter((num) => num> 40)
+
+// console.log(values) // output -> [ 41, 51, 61, 71, 81, 91 ]
+
+
